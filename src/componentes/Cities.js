2a44ed44f8947d0638e271b8cc6429901b44e830
jeanney11 <jclayrz11@gmail.com>
2022-01-20 02:02:08 -0300

    Search

--- src/componentes/Cities.js
+++ src/componentes/Cities.js
@@ -2,25 +2,23 @@ import React from "react";
 
 import Carousel from "../componentes/Carousel";
 import CardSlide from "./CardSlide";
-import CardsCity from "../componentes/CardsCity"
-
+import CardsCity from "../componentes/CardsCity";
+import Search from "./Search";
 import SignIn from "../componentes/SignIn";
 import SignUp from "../componentes/signUp";
 
+function Cities() {
+  return (
+    <div>
+      <Search />
 
-
-function Cities(){
-
-    return(
-         <div>
-        <Carousel/>
-    
-    
-        
-        <CardsCity/>        
-        
-        </div>
-    )
+      <CardsCity />
+      <div class="header">
+        <p>Aprovecha Nuestra Promoción</p>
+      </div>
+      <Carousel />
+    </div>
+  );
 }
 
 export default Cities;
---
+++ src/componentes/Search.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+
+function Search(){
+
+    return(
+        <div class="wrapper">
+	<div class="container">
+    <div class="search_wrap search_wrap_6">
+			<div class="search_box">
+				<input type="text" class="input" placeholder="search..."/>
+				<div class="btn">
+					<p>Search</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+    );
+}
+
+export default Search;
