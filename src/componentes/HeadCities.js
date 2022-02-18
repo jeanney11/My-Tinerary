38d217d7b0b03fb8d40dc966135e4b33daa852d8
jeanney11 <jclayrz11@gmail.com>
2022-02-18 20:17:24 -0300

    Conexion de Data y Itinerarios

---
+++ src/componentes/HeadCities.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeadCities() {
+
+    
+  return (
+    <div class="containerSearch">
+  <input type="textSearch" placeholder="Search...">
+  <div class="searchDos"></div></input>
+</div>
+    
+  );
+}
+
+export default HeadCities;
